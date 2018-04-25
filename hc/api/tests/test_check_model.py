8f513b55084ed1c7230c1f6d1f6e630ed446a773
Collins Njau <collinsnjau39@gmail.com>
2018-04-25 12:26:33 +0300

    [Feature #156687690] Fix test_check_model tests

--- hc/api/tests/test_check_model.py
+++ hc/api/tests/test_check_model.py
@@ -9,10 +9,13 @@ class CheckModelTestCase(TestCase):
 
     def test_it_strips_tags(self):
         check = Check()
+        
 
         check.tags = " foo  bar "
         self.assertEquals(check.tags_list(), ["foo", "bar"])
-        ### Repeat above test for when check is an empty string
+        check.tags = "        "
+        ### Repeat above test for when check is an empty string DONE
+        self.assertEquals(check.tags_list(), [])
 
     def test_status_works_with_grace_period(self):
         check = Check()
@@ -23,7 +26,7 @@ class CheckModelTestCase(TestCase):
         self.assertTrue(check.in_grace_period())
         self.assertEqual(check.get_status(), "up")
 
-        ### The above 2 asserts fail. Make them pass
+        ### The above 2 asserts fail. Make them pass DONE
 
     def test_paused_check_is_not_in_grace_period(self):
         check = Check()
@@ -35,4 +38,7 @@ class CheckModelTestCase(TestCase):
         check.status = "paused"
         self.assertFalse(check.in_grace_period())
 
-    ### Test that when a new check is created, it is not in the grace period
+        check.status= "new"
+        self.assertFalse(check.in_grace_period())
+
+    ### Test that when a new check is created, it is not in the grace period DONE
