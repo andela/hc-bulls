e485163ed74f0e31f422b20af4d916ae8c58a42f
Collins Njau <collinsnjau39@gmail.com>
2018-05-02 19:42:30 +0300

    Add styling to the FAQS page

--- static/js/faq.js
+++ static/js/faq.js
@@ -1,7 +1,6 @@
 $(function(){
     var x =$("#firstQuestion")
-    x.hide()
-    
+    x.hide() 
     $("#first_button").click(function(){
         var x =$("#firstQuestion")
         x.toggle()       
