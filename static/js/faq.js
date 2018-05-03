cf1a5310ed5d65121589d7c7b9e999a88c31bad7
Collins Njau <collinsnjau39@gmail.com>
2018-05-03 15:02:31 +0300

    Add more questions for the FAQ section

--- static/js/faq.js
+++ static/js/faq.js
@@ -1,15 +1,16 @@
 $(function(){
-    var x =$("#firstQuestion,#secondQuestion")
+    var x =$("#firstQuestion,#secondQuestion,#thirdQuestion")
     x.hide()
-
-    
-
     $("#first_button").click(function(){
         var x =$("#firstQuestion")
         x.toggle()       
     })
     $("#second_button").click(function(){
         var y =$("#secondQuestion")
+        y.toggle()       
+    })
+    $("#third_button").click(function(){
+        var y =$("#thirdQuestion")
         y.toggle()       
     })
 })
