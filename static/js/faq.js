1068fabe833ccbaadbb8de32a8fa24ff884b08f5
Collins Njau <collinsnjau39@gmail.com>
2018-05-03 14:29:47 +0300

    Add javascript to toggle the faq

--- static/js/faq.js
+++ static/js/faq.js
@@ -1,8 +1,15 @@
 $(function(){
-    var x =$("#firstQuestion")
-    x.hide() 
+    var x =$("#firstQuestion,#secondQuestion")
+    x.hide()
+
+    
+
     $("#first_button").click(function(){
         var x =$("#firstQuestion")
         x.toggle()       
+    })
+    $("#second_button").click(function(){
+        var y =$("#secondQuestion")
+        y.toggle()       
     })
 })
