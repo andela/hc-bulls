cd89a42ada079c6ae6b2c0d6f6eeb5b6ad606b14
Collins Njau <collinsnjau39@gmail.com>
2018-05-04 09:05:24 +0300

    Add more FAQS and videos

--- static/js/faq.js
+++ static/js/faq.js
@@ -1,5 +1,5 @@
 $(function(){
-    var x =$("#firstQuestion,#secondQuestion,#thirdQuestion")
+    var x =$("#firstQuestion,#secondQuestion,#thirdQuestion,#fourthQuestion,#fifthQuestion,#sixthQuestion,#seventhQuestion,#eighthQuestion,#ninethQuestion")
     x.hide()
     $("#first_button").click(function(){
         var x =$("#firstQuestion")
@@ -11,6 +11,30 @@ $(function(){
     })
     $("#third_button").click(function(){
         var y =$("#thirdQuestion")
+        y.toggle()       
+    })
+    $("#fourth_button").click(function(){
+        var y =$("#fourthQuestion")
+        y.toggle()       
+    })
+    $("#fifth_button").click(function(){
+        var y =$("#fifthQuestion")
+        y.toggle()       
+    })
+    $("#sixth_button").click(function(){
+        var y =$("#sixthQuestion")
+        y.toggle()       
+    })
+    $("#seventh_button").click(function(){
+        var y =$("#seventhQuestion")
+        y.toggle()       
+    })
+    $("#eighth_button").click(function(){
+        var y =$("#eighthQuestion")
+        y.toggle()       
+    })
+    $("#nineth_button").click(function(){
+        var y =$("#ninethQuestion")
         y.toggle()       
     })
 })
