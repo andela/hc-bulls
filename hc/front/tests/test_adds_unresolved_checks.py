548b348250e4917eb56fa9de45afaa73f4a95d1b
murageden <muragemd@gmail.com>
2018-05-10 10:08:15 +0300

    Add test it returns the page for unresolved checks

--- hc/front/tests/test_adds_unresolved_checks.py
+++ hc/front/tests/test_adds_unresolved_checks.py
@@ -10,7 +10,12 @@ class MyChecksTestCase(BaseTestCase):
 		self.check = Check(user=self.alice, name="A simple test ping", timeout=td(seconds=60), grace=td(seconds=60))
 		self.check.save()
 
-	def test_it_creates_the_check(self):
+	def test_it_created_the_check(self):
 		self.client.login(username='alice@example.org', password='password')
 		response = self.client.get("/checks/")
 		self.assertContains(response, "A simple test ping", status_code=200)
+
+	def test_it_contains_message_for_no_unresolved_check(self):
+		self.client.login(username='alice@example.org', password='password')
+		response = self.client.get("/checks/unresolved")
+		sel.assertContains(response, "You don't have any checks that have not been resolved yet", status_code=200)
