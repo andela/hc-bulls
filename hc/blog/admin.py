25b8156b06673b1e7bbd4bbdfef512b0d4cbd20d
Collins Njau <collinsnjau39@gmail.com>
2018-05-11 13:30:42 +0300

    Add the models to the admin site

--- hc/blog/admin.py
+++ hc/blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from hc.blog.models import BlogCategory,BlogPost
 # Register your models here.
+admin.site.register(BlogCategory)
+admin.site.register(BlogPost)
--- hc/blog/models.py
+++ hc/blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class BlogCategory(models.Model):
+    title=models.CharField(max_length=300)
+
+
+    def __str__(self):
+        return self.title
+
+
+class BlogPost(models.Model):
+    author = models.ForeignKey(User, blank=True, null=True)
+    title = models.CharField(max_length=300, blank = False)
+    content = models.TextField(blank = False)
+    category = models.ForeignKey(BlogCategory, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_on']
+        verbose_name = "Blog Post"
+        verbose_name_plural = "Blog posts"
+
+    def __str__(self):
+        return self.title        
