e3843073075f4e80824e0e6462359d06a3f5d5be
Mochualex4 <Mochualex4@gmail.com>
2018-05-13 19:18:58 +0300

    specify 404 and 500 urls

--- hc/urls.py
+++ hc/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from hc.accounts import views
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('hc.accounts.urls')),
@@ -8,3 +9,6 @@ urlpatterns = [
     url(r'^', include('hc.front.urls')),
     url(r'^', include('hc.payments.urls'))
 ]
+
+handler404 = views.Error404View.as_error_view()
+handler500 = views.Error500View.as_error_view()
